fbfde3d111143656352b0a5d9e7d3acf415b3b0b
Dimitris Niras <niras.94@gmail.com>
2024-09-14 00:29:34 +0300

    Implement graceful shutdown for the API (#24)

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"context"
+	"net/http"
 	"os"
+	"os/signal"
 	"simpler-products/config"
 	"simpler-products/routers"
+	"syscall"
+	"time"
 
 	"github.com/sirupsen/logrus"
 )
@@ -19,12 +24,39 @@ func main() {
 		log.Fatal(err)
 	}
 
+	router := routers.NewRouter(cfg.Services, log)
+
+	// Create a server instance
+	srv := &http.Server{
+		Addr:    ":" + cfg.Port,
+		Handler: router,
+	}
+
+	// Start the server in a goroutine
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shut down the server with a timeout of 5 seconds.
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGABRT)
+	<-quit
+	log.Println("Shutting down server...")
+
 	// close Database connection when app terminates
 	defer cfg.DB.Close()
 	defer log.Debug("Closing Database connection")
 
-	router := routers.NewRouter(cfg.Services, log)
+	// Create a context with a timeout
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 
-	log.Printf("Server listening on :%s", cfg.Port)
-	log.Fatal(router.Run(":" + cfg.Port))
+	// Attempt graceful shutdown
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
 }
